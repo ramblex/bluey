7767bab3966439d78b84cb7d2986a086785ba374
Alex Duller <alexduller@gmail.com>
2011-04-19 21:37:22 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bluey::Application.config.secret_token = '49a5179f85eb4db8c63781b95fa15888478ae8e251570a7db728deebbe3bfa276c9569c6d364a8b66f59c37eca580d13b1ecc75ea752bd228be409dbd756715f'
