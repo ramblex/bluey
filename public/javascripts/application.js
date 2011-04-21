c1e17e70dbdda3e3653fe5453eecee84798e371b
Alex Duller <alexduller@gmail.com>
2011-04-22 00:27:42 +0100

    Added stats-bar stuff

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,12 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+function animate_progress(meter_id, percentage) {
+  var bar = $('.progress:eq('+meter_id+')');
+  bar.animate({width: percentage + '%'}, 'slow');
+}
+
+$(document).ready(function() {
+  animate_progress(0, 30);
+  animate_progress(1, 50);
+});
