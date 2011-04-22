b6ef512728abde3b3ac65b8cc8f3df1e76d85cb0
Alex Duller <alexduller@gmail.com>
2011-04-22 01:18:54 +0100

    Changed progress bars to meters

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,12 +1,24 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function animate_progress(meter_id, percentage) {
-  var bar = $('.progress:eq('+meter_id+')');
-  bar.animate({width: percentage + '%'}, 'slow');
+function animate_progress(meter_id, percentage, append) {
+  var meter = $('.meter:eq('+meter_id+')');
+  var stick =  $('img', meter);
+  stick.rotate(-180);
+  stick.rotate({animateTo: ((percentage / 100) * 180) - 180});
+  var hint = $('<div class="hint">'+percentage+'%</div>');
+  if (append === true) {
+    meter.append(hint);
+    meter.hover(function() {
+      hint.fadeIn();
+    },
+    function() {
+      hint.fadeOut();
+    });
+  }
 }
 
 $(document).ready(function() {
-  animate_progress(0, 30);
-  animate_progress(1, 50);
+  animate_progress(0, 30, true);
+  animate_progress(1, 50, false);
 });
