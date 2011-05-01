8474e4596732ca089f30d4b6cb0da4f175c16a49
Alex Duller <alexduller@gmail.com>
2011-05-01 13:52:41 +0100

    Only update user goals from the form that was submitted

--- app/controllers/user_goals_controller.rb
+++ app/controllers/user_goals_controller.rb
@@ -9,8 +9,8 @@ class UserGoalsController < ApplicationController
     @user_goals = current_user.user_profile.user_goals
     @errors = false
     @user_goals.each do |user_goal|
-      logger.info "goals: #{params[:user_goals][user_goal.id.to_s].inspect}"
-      if !user_goal.update_attributes(params[:user_goals][user_goal.id.to_s])
+      goals = params[:user_goals][user_goal.id.to_s]
+      if !goals.nil? && !user_goal.update_attributes(params[:user_goals][user_goal.id.to_s])
         @errors = true
       end
     end
@@ -20,7 +20,7 @@ class UserGoalsController < ApplicationController
         format.html { redirect_to user_goals_path, :notice => 'Updated your goals' }
       else
         format.js
-        format.html { render :action => 'index', :alert => 'Could not update your goals' }
+        format.html { render :action => 'index', :alert => 'Could not update your stats' }
       end
     end
   end
