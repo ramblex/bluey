862936a298206ceb6043c529a19498255e78584f
Alex Duller <alexduller@gmail.com>
2011-05-10 21:15:28 +0100

    Deleted test

--- test/functional/pages_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class PagesControllerTest < ActionController::TestCase
-  test "should get landing" do
-    get :landing
-    assert_response :success
-  end
-
-end
--- test/unit/user_test.rb
+++
@@ -1,8 +0,0 @@
-require 'test_helper'
-
-class UserTest < ActiveSupport::TestCase
-  test "should not save without email" do
-    user = User.new
-    assert !user.save, "Saved user without email"
-  end
-end
