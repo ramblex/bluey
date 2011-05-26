f0ea2c6098bc4eee9fd6aae516f00de453720360
Alex Duller <alexduller@gmail.com>
2011-05-26 19:55:56 +0100

    Fixed adding days

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,12 +1,11 @@
 class DaysController < ApplicationController
   def create
     plan = Plan.find(params[:plan_id])
-    next_day_number = (plan.days.empty?) ? 1 : plan.days.maximum("number") + 1
-    @day = Day.new(:plan => plan, :number => next_day_number)
+    @day = Day.new(:plan => plan)
     respond_to do |format|
       if @day.save
         format.html { redirect_to :back, :notice => 'Added day to your plan' }
-        format.js
+        format.js { @day_count = plan.days.count }
       else
         format.html { redirect_to :back, :alert => 'Could not add a day' }
         format.js
